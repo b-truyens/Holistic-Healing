0b7b9f68ae1d4cb5215705e0e4ecc7ab9e8eae63
B3- Boris <99405091+b-truyens@users.noreply.github.com>
2023-09-26 13:33:08 +0200

    Create maak_mappen.sh

---
+++ maak_mappen.sh
@@ -0,0 +1,48 @@
+# 🌲 Root Bound Coöperatie 🌲
+
+## Manifesto
+
+### Inleiding
+In de snel veranderende wereld van vandaag is de behoefte aan verbinding met de natuur en authentieke zelfzorg nooit groter geweest. De Root Bound Coöperatie is geboren uit een passie voor holistisch welzijn, duurzaamheid en gemeenschapsopbouw.
+
+### 🌍 Missie
+Wij streven ernaar om een brug te slaan tussen traditionele wijsheid en moderne levensstijlen, en zo een harmonieuze balans te creëren tussen mens en natuur. Onze missie is om mensen te empoweren, te verbinden en te inspireren om actieve deelnemers te worden in hun eigen welzijn en dat van de planeet.
+
+### 🌿 Visie
+Onze visie is een wereld waarin elk individu toegang heeft tot de hulpmiddelen, kennis en gemeenschap die nodig zijn om een leven van welzijn, vreugde en duurzaamheid te leiden.
+
+### 🌾 Kernwaarden
+- **Diepe Verbinding**: Wij geloven dat ware genezing en welzijn voortkomen uit een diepe verbinding met de natuur en onszelf.
+- **Duurzaamheid**: Wij zijn toegewijd aan het bevorderen van duurzame praktijken die onze planeet beschermen en herstellen.
+- **Gemeenschap**: Wij geloven in de kracht van gemeenschap en samenwerking om positieve verandering te bewerkstelligen.
+
+## 🌍 [Projecten](./projecten)
+
+### 1. [Natuurlijke Geneeswijzen Workshops](./projecten/natuurlijke-geneeswijzen-workshops) 🌿
+   - **Beschrijving**: Maandelijkse workshops waarin experts in natuurlijke geneeswijzen hun kennis en vaardigheden delen over kruidengeneeskunde, aromatherapie, en meer.
+
+### 2. [Duurzame Landbouw Initiatieven](./projecten/duurzame-landbouw-initiatieven) 🌾
+   - **Beschrijving**: Samenwerking met lokale boeren om duurzame landbouwpraktijken te bevorderen, inclusief permacultuur en biodynamische landbouw.
+
+### 3. [Meditatie en Yoga Retreats](./projecten/meditatie-yoga-retreats) 🧘
+   - **Beschrijving**: Jaarlijkse retraites in serene natuurgebieden, gericht op diepe innerlijke transformatie en verbinding met de natuur.
+
+### 4. [Eco-Bouw Workshops](./projecten/eco-bouw-workshops) 🏡
+   - **Beschrijving**: Workshops over duurzame bouwtechnieken, zoals aarden huizen en strobalen bouw.
+
+### 5. [Wildpluk Excursies](./projecten/wildpluk-excursies) 🍄
+   - **Beschrijving**: Geleide tochten in lokale natuurgebieden om eetbare planten en paddenstoelen te leren kennen.
+
+### 6. [Groene Technologie Demonstraties](./projecten/groene-technologie-demonstraties) 💡
+   - **Beschrijving**: Demonstraties van de nieuwste groene technologieën, zoals zonne-energie en regenwateropvang.
+
+### 7. [Kunst en Ambacht van de Natuur](./projecten/kunst-ambacht-natuur) 🎨
+   - **Beschrijving**: Creatieve workshops waarin deelnemers leren om kunst en ambachten te maken met natuurlijke materialen.
+
+### 8. [Kinderen in de Natuur Programma](./projecten/kinderen-natuur-programma) 🧒
+   - **Beschrijving**: Educatieve activiteiten en kampen voor kinderen, gericht op natuurbehoud en -bewustzijn.
+
+### 9. [Zero Waste Workshops](./projecten/zero-waste-workshops) ♻️
+   - **Beschrijving**: Workshops over afvalvrije praktijken, compostering en duurzaam leven.
+
+### 10. [Community Healing Circles](./projecten/community-healing-circles)
